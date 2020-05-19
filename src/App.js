9b0fe698f49cf3f93ca8ba0b9006346e93ad8187
= <leoncai197@gmail.com>
2020-05-19 13:58:22 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { HashRouter, Route } from "react-router-dom"
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Heatmap from './Heatmap'
 
 class App extends Component {
--- src/Navigation.js
+++ src/Navigation.js
@@ -7,7 +7,7 @@ class Navigation extends Component {
     render() {
         return (
             <>
-              <Navbar>
+              {/* <Navbar>
                 <Navbar.Brand className="nav-brand" style={{ textDecoration: 'none' }} href="/">   
                   <img src={Logo} className="logo-button" style={{width: 27}} /> 
                   Workout Heatmap
@@ -15,6 +15,20 @@ class Navigation extends Component {
                 <Nav.Link className="nav-item" style={{ textDecoration: 'none', float: "right" }} href="/">About</Nav.Link>
                 <Nav.Link className="nav-item" style={{ textDecoration: 'none', float: "right" }} href="/">Stats</Nav.Link>
                 <Nav.Link className="nav-item" style={{ textDecoration: 'none', float: "right" }} href="/">Map</Nav.Link>
+              </Navbar> */}
+              <Navbar collapseOnSelect expand="lg" className="color-nav" variant="dark">
+                <Navbar.Brand className="nav-brand" href="/">
+                  <img src={Logo} className="logo-button" style={{width: 27}} /> 
+                  Workout Heatmap
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="ml-auto">
+                    <Nav.Link className="nav-item" href="/">Map</Nav.Link>
+                    <Nav.Link className="nav-item" href="/">Stats</Nav.Link>
+                    <Nav.Link className="nav-item" href="/">About</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
               </Navbar>
             </>
         )
