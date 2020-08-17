9a207c8624b8f68b3637234b4e9e3bd65569f686
= <leoncai197@gmail.com>
2020-08-17 02:11:48 -0500

    Reverting back to Heroku URL until an SSL certificate is present for custom domain

--- src/Pages/Heatmap.js
+++ src/Pages/Heatmap.js
@@ -18,7 +18,7 @@ const mapStyles = {
 };
 
 // url for production is https://www.workoutheatmap.me/, url for development is http://localhost:3000/
-const base_url = "https://www.workoutheatmap.me/"
+const base_url = "https://workout-heatmap.herokuapp.com/"
 
 class Heatmap extends Component {
 
